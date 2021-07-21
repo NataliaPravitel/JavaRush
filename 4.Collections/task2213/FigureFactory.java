60b2eb0ea1e4f4338d692b207ca97c0a5dc8b485
Natalia Pravitel <natalia.lvova1990@gmail.com>
2021-07-21 21:13:37 +0300

    2213 game Tetris

---
+++ 4.Collections/task2213/FigureFactory.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task22.task2213;
+//Тетрис(11)
+//Теперь создай класс FigureFactory.
+//С его помощью мы будем создавать фигуры различных форм.
+//Пока он будет содержать только один статический метод createRandomFigure:
+//static Figure createRandomFigure(int x,int y)
+//Реализацию метода мы напишем позже, а сейчас для того, чтобы программа скомпилировалась,
+// верни в методе какое-нибудь значение, например, null.
+//
+//
+//Requirements:
+//1. Класс FigureFactory должен быть создан в отдельном файле.
+//2. В классе FigureFactory должен быть создан метод createRandomFigure с двумя параметрами типа int.
+//3. Метод createRandomFigure должен быть статическим.
+//4. Должна существовать возможность получить доступ к методу createRandomFigure извне класса FigureFactory.
+//5. Метод createRandomFigure должен иметь тип возвращаемого значения Figure.
+
+public class FigureFactory {
+  public static Figure createRandomFigure(int x,int y) {
+    return null;
+  }
+}
